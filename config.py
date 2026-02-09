054fd96d7a1100cac723e76bb397b3db3f72b21c
Oleg Rubtsov <rubtsovoa@gmail.com>
2026-02-09 20:25:11 +0400

    add functions implements

--- config.py
+++ config.py
@@ -4,13 +4,16 @@ from ym_fields import hits
 
 load_dotenv()
 
-OATH_TOKEN = os.getenv("OATH_TOKEN")
-COUNTER_ID = os.getenv("COUNTER_ID")
+OATH_TOKEN = os.getenv("OATH_TOKEN") or ''
+COUNTER_ID = os.getenv("COUNTER_ID") or ''
 
 API_URL = (
     f"https://api-metrika.yandex.net/management/v1/counter/{COUNTER_ID}/logrequests"
 )
 
-HEADERS = {}
+HEADERS = {
+    'Authorization': f'OAuth {OATH_TOKEN}',
+    'Content-Type': 'application/x-yametrika+json'
+}
 
 PARAMS = {"fields": ",".join(hits)}
--- main.py
+++ main.py
@@ -2,15 +2,23 @@ import pandas as pd
 import io
 import requests
 import json
-from config import API_URL
+from config import API_URL, HEADERS, PARAMS
 
 
 def get_logs():
-    pass
+    response = requests.get(API_URL, headers=HEADERS)
+    return json.loads(response.text)
 
 
 def create_log_request():
-    pass
+    params = {
+        "date1": "2025-12-01",
+        "date2": "2025-12-31",
+        "fields": PARAMS['fields'],
+        "source": "hits",
+    }
+    response = requests.post(API_URL, headers=HEADERS, params=params)
+    print(response.text)
 
 
 def remove_log(request_id: int):
@@ -18,4 +26,11 @@ def remove_log(request_id: int):
 
 
 def download_log(request_id: int):
-    pass
+    with open(f'data/log_{request_id}.csv', 'w') as file:
+        pass
+    url = f'{API_URL}/{request_id}'
+
+print(get_logs())
+request_id = 51427266
+# create_log_request()
+
